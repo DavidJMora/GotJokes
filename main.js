66b89d40a9c03a9ffdbfd1a98c5a1832dfa5de8f
David Mora <David.mora@codeimmersives.com>
2019-04-23 15:23:24 -0400

    made a second button that only generates random jokes within the nerdy category

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 window.onload = init;
 
 function init() {
-    document.querySelector('.button')
-        .addEventListener('click', sendRequest);
-
-    sendRequest(); 
+    document.querySelector('#random')
+        .addEventListener('click', randomJoke);
+    
+    document.querySelector('#nerdy-jokes')
+        .addEventListener('click', nerdyJokes);
+ 
 }
-function sendRequest() {
+function randomJoke() {
     event.preventDefault();
-    const xhr = new XMLHttpRequest();
     const url = 'http://api.icndb.com/jokes/random?escape=javascript';
+    
+    sendRequest(url)
+}
+
+function sendRequest(url) {
+    event.preventDefault;
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload = handleData;
     xhr.send();
 }
+
 function handleData(event) {
     const rawData = JSON.parse(event.target.responseText);
     const {value} = rawData;
     const {joke} = value;
     document.querySelector('#joke').innerText = joke;
+    console.log(joke);
+}
+
+function nerdyJokes() {
+    const url = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]';
+    sendRequest(url)
 }
